e0537b6073164743f61484b90b2e34f871fd7991
Aamir041 <aamirsaudagar34@gmail.com>
2023-08-27 15:08:12 +0530

    added new changes

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,12 +4,9 @@ const Home = () => {
     return (
         <div className="home">
             <div className="intro">
-                <h3 className="intro1">Hello, my name is aamir</h3>
-                <h1 className="intro2">I make Websites.</h1>
-            </div>
-
-            <div className="hi">
-                <img src={hi}/>
+                <h3 className="intro1">Hello, I'm</h3>
+                <h1 className="intro2">Aamir Saudagar</h1>
+                <p>Creative Web Developer</p>
             </div>
             
         </div>
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,36 +1,42 @@
-import ProjectCard from "../../lib/ProjectCard/ProjectCard"
-import "./Projects.css"
+import ProjectCard from "../../lib/ProjectCard/ProjectCard";
+import "./Projects.css";
+import exprenseTrackerImg from "../../assets/projectsImg/expense-tracker/img.png";
+import blogRecipeSiteImg from "../../assets/projectsImg/add-your-recipe/img.png";
+import memoryCardGameImg from "../../assets/projectsImg/memory-card-game/img.png";
+import todoImg from "../../assets/projectsImg/todo/img.png";
+import restaurantImg from "../../assets/projectsImg/restaurant-homepage/img.png";
+import cmsImg from "../../assets/projectsImg/nerd-cms/img.png"
 
 const Projects = () => {
     
     const projects = [
         {
             projectName: "React Expense Tracker",
-            img:"https://github.com/Aamir041/portfolio-webiste/blob/main/src/assets/projectsImg/expense-tracker/img.png?raw=true",
-            link:"https://github.com/Aamir041/portfolio-webiste/blob/main/src/assets/projectsImg/expense-tracker/img.png?raw=true",
+            img:exprenseTrackerImg,
+            link:"https://github.com/Aamir041/React-Expense-Tracker",
             skills:["JavaScript","HTML","CSS"],
         },
         {
             projectName: "Add Your Recipe Site",
-            img:"https://github.com/Aamir041/portfolio-webiste/blob/main/src/assets/projectsImg/add-your-recipe/img.png?raw=true",
+            img:blogRecipeSiteImg,
             link:"https://github.com/Aamir041/add-your-recipe-site",
             skills:["JavaScript","HTML","CSS","json-server"]
         },
         {
             projectName: "Memory Card",
-            img:"https://github.com/Aamir041/portfolio-webiste/blob/main/src/assets/projectsImg/memory-card-game/img.png?raw=true",
+            img: memoryCardGameImg,
             link:"https://github.com/Aamir041/memory_game_project_react",
             skills:["JavaScript","HTML","CSS"]
         },
         {
             projectName: "Todo List",
-            img:"https://github.com/Aamir041/portfolio-webiste/blob/main/src/assets/projectsImg/todo/img.png?raw=true",
+            img:todoImg,
             link:"https://github.com/Aamir041/todo-list",
             skills:["JavaScript","HTML","CSS"]
         },
         {
             projectName: "Restaurant",
-            img:"https://github.com/Aamir041/portfolio-webiste/blob/main/src/assets/projectsImg/restaurant-homepage/img.png?raw=true",
+            img:restaurantImg,
             link:"https://github.com/Aamir041/cool-restaurent-site",
             skills:["HTML","CSS"]
         },
@@ -42,7 +48,7 @@ const Projects = () => {
         },
         {
             projectName: "Nerd CMS",
-            img:"https://github.com/Aamir041/portfolio-webiste/blob/main/src/assets/projectsImg/nerd-cms/img.png?raw=true",
+            img:cmsImg,
             link:"https://github.com/Aamir041/Nerd-CMS",
             skills:["Linux","Docker","YAML"]
         },
