69c3091de7f8bd26f323105a4ed1e4270cc3b51d
Aamir041 <aamirsaudagar34@gmail.com>
2024-07-14 21:03:54 +0530

    added blog some csss

--- src/components/BlogCard/BlogCard.jsx
+++ src/components/BlogCard/BlogCard.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import "./BlogCard.css"
 export default function BlogCard() {
 
-  const blog = {__html:`<h1 id="sample-markdown">Sample Markdown</h1> <p>This is some basic, sample markdown.</p> <h2 id="second-heading">Second Heading</h2> <ul> <li>Unordered lists, and:<ol> <li>One</li> <li>Two</li> <li>Three</li> </ol> </li> <li>More</li> </ul> <blockquote> <p>Blockquote</p> </blockquote> <p>And <strong>bold</strong>, <em>italics</em>, and even <em>italics and later <strong>bold</strong></em>. Even <del>strikethrough</del>. <a href="https://markdowntohtml.com">A link</a> to somewhere.</p> <p>And code highlighting:</p> <pre><code class="lang-js"><span class="hljs-keyword">var</span> foo = <span class="hljs-string">'bar'</span>; <span class="hljs-function"><span class="hljs-keyword">function</span> <span class="hljs-title">baz</span><span class="hljs-params">(s)</span> </span>{ <span class="hljs-keyword">return</span> foo + <span class="hljs-string">':'</span> + s; } </code></pre> <p>Or inline code like <code>var foo = &#39;bar&#39;;</code>.</p> <p>Or an image of bears</p> <p><img src="http://placebear.com/200/200" alt="bears"></p> <p>The end ...</p>`}
+  const blog = {__html:`<h1 id="sample-markdown">Sample Markdown</h1> <p>This is some basic, sample markdown.</p> <h2 id="second-heading">Second Heading</h2> <ul> <li>Unordered lists, and:<ol> <li>One</li> <li>Two</li> <li>Three</li> </ol> </li> <li>More</li> </ul> <blockquote> <p>Blockquote</p> </blockquote> <p>And <strong>bold</strong>, <em>italics</em>, and even <em>italics and later <strong>bold</strong></em>. Even <del>strikethrough</del>. <a href="https://markdowntohtml.com">A link</a> to somewhere.</p>  <p>Or an image of bear</p> <p><img src="http://placebear.com/200/200" alt="bears"></p> <p>The end ...</p>`}
 
 
 
   return (
     <div className='blog-card'>
-      <h1 className='blog-title'>Blog Title</h1>
-      <p className='blog-date'><em>18-08-2001</em></p>
+      <h1 className='blog-title'>Simple HTML be like</h1>
+      <p className='blog-date'><em></em>14-07-2024</p>
       <div className='blog-content' dangerouslySetInnerHTML={blog} />
     </div>
   )
