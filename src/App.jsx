5c3f00f09f0c88d0350c5a51dbf86f7941b59e51
Aamir041 <aamirsaudagar34@gmail.com>
2026-02-04 20:17:48 +0530

    stipped out unused imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import "./App.css";
 import About from "./components/About/About";
-import BlogCard from "./components/BlogCard/BlogCard";
-import Blogs from "./components/Bolgs/Blogs";
 import Contact from "./components/Contact/Contact";
 import Home from "./components/Home/Home";
 import Projects from "./components/Projects/Projects";
@@ -21,8 +19,6 @@ const App = () => {
             <Route path="/about" element={<About />} />
             <Route path="/projects" element={<Projects />} />
             <Route path="/contact" element={<Contact />} />
-            <Route path="/blogs" element={<Blogs/>} />
-            <Route path="/blog/:blogId" element={<BlogCard/>} />
             <Route path="*" element={<Home />} />
           </Routes>
         </div>
